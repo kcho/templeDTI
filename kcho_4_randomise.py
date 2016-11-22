1187eeb806ad2216aac1eb037407a9626edc9186
kcho <cke8671@hotmail.com>
2016-11-22 13:29:00 +0900

    subject number added to the matrix; directory reedit; gets inputs

--- kcho_4_randomise.py
+++ kcho_4_randomise.py
@@ -1,55 +1,83 @@
 #!/ccnc_bin/venv/bin/python
 import os
 import re
-
-skeletonDirectories = [
-    'skeleton_images/splitSkeleton/post_m_pre']
-    #'skeleton_images/splitSkeleton/pre_m_post']
-
-def main():
-    for skeletonDirectory in skeletonDirectories:
-        design_con='''/NumWaves 2
-    /NumContrasts 2
-    /PPheights 1 1
-    /Matrix
-    1 -1
-    -1 1
-    '''
-        design_mat='''/NumWaves 2
-    /NumPoints 46
-    /PPheights 1 1
-    /Matrix
-    {0}{1}'''.format('1 0\n'*17,'0 1\n'*29)
+import argparse
+import textwrap
+import difflib
 
 
+def main(args):
+    dataDir  = args.dir
+    skeletonDirectory = os.path.join(dataDir,
+            'skeleton_images_'+args.measure,
+            'splitSkeleton',
+            'post_m_pre')
+
+    design_con='''/NumWaves 2
+/NumContrasts 2
+/PPheights 1 1
+/Matrix
+1 -1
+-1 1
+'''
+
+    subjectOrigFiles = [x for x in os.listdir(dataDir+'/origdata') if x.endswith('nii.gz')]
+    groups = list(set([x[:3] for x in subjectOrigFiles]))
+    groups.sort()
+
+    # divided by two because it's longitudinal design
+    subjectNum = len(subjectOrigFiles) / 2
+    firstGroupNum = len([x for x in subjectOrigFiles if x.startswith(groups[0])]) / 2
+    secondGroupNum = len([x for x in subjectOrigFiles if x.startswith(groups[1])]) / 2
+
+    design_mat='''/NumWaves 2
+/NumPoints {subjectNum}
+/PPheights 1 1
+/Matrix
+{firstGroupMat}{secondGroupMat}'''.format(subjectNum=subjectNum,
+                                        firstGroupMat = '1 0\n'*firstGroupNum,
+                                        secondGroupMat = '0 1\n'*secondGroupNum)
+
+    if os.path.isfile(skeletonDirectory+'/design.con'):
+        print skeletonDirectory+'/design.con exists ! Please remove or rename design.con'
+    else:
         with open(skeletonDirectory+'/design.con','w') as f:
             f.write(design_con)
         with open(skeletonDirectory+'/design.mat','w') as f:
             f.write(design_mat)
 
-
-    #command='randomise \
-            #-i {skeletonDirectory}/pre_minus_post_merged.nii.gz \
-            #-o {skeletonDirectory}/tbss_pre_m_post \
-            #-m fa_collection/stats/mean_FA_skeleton_mask \
-            #-d {skeletonDirectory}/design.mat \
-            #-t {skeletonDirectory}/design.con \
-            #-n 5000 \
-            #--T2'.format(skeletonDirectory=skeletonDirectories[1])
-
-    #os.system(command)
-
     command='randomise \
-            -i {skeletonDirectory}/post_minus_pre_merged.nii.gz \
-            -o {skeletonDirectory}/tbss_post_m_pre \
-            -m fa_collection/stats/mean_FA_skeleton_mask \
-            -d {skeletonDirectory}/design.mat \
-            -t {skeletonDirectory}/design.con \
-            -n 5000 \
-            --T2'.format(skeletonDirectory=skeletonDirectories[0])
+-i {skeletonDirectory}/post_minus_pre_merged.nii.gz \
+-o {skeletonDirectory}/tbss_post_m_pre \
+-m {dataDir}/stats/mean_FA_skeleton_mask \
+-d {skeletonDirectory}/design.mat \
+-t {skeletonDirectory}/design.con \
+-n 5000 \
+--T2'.format(skeletonDirectory=skeletonDirectory, dataDir = dataDir)
 
+    print command
     os.system(command)
 
-
 if __name__=='__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+            {codeName} : Preprocess skeleton images for longitudinal design TBSS
+            ========================================
+            eg) {codeName} -s /Users/kevin/TBSS
+                where there are 'stats' and 'FA' directories.
+            '''.format(codeName=os.path.basename(__file__))))
+    parser.add_argument(
+        '-d', '--dir',
+        help='Data directory location, default=pwd',
+        default=os.getcwd())
+    parser.add_argument(
+        '-m', '--measure',
+        help='"FA", "AD", "MD" or "RD"',
+        default="FA")
+    args = parser.parse_args()
+
+    if not args.dir:
+        parser.print_help()
+    else:
+        main(args)
