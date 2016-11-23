2f3cce19a8907bb1f1134d5fd627242481fe5969
kcho <sky8671@gmail.com>
2016-11-23 21:25:52 +0900

    ROI extraction option being added

--- kcho_6_roi.py
+++ kcho_6_roi.py
@@ -12,20 +12,25 @@ import pandas as pd
 
 
 def main(args):
-    ROIdir = join(args.dir, 'ROIs')
+
+    if lower(args.template) != 'mni':
+        ROIdir = join(args.dir, 'ROIs_template_reg')
+    else:
+        ROIdir = join(args.roidir)
+
     mergedMap = join(args.dir, 'stats', 'all_{0}.nii.gz'.format(args.measure))
-    splitDir = join(args.dir, 'FA_registered')
+    splitDir = join(args.dir, '{0}_registered'.format(args.measure))
 
     makeDirectory(splitDir)
     if 'all_{measure}.nii.gz'.format(measure=args.measure) not in os.listdir(splitDir):
-        print '-'*10,'copying all_{0}_skeletonised'.format(args.measure),'-'*10
+        print '-'*10,'copying all_{0}'.format(args.measure),'-'*10
         shutil.copy(mergedMap, splitDir)
 
     if 'split0091' not in os.listdir(splitDir):
         copied_mergedMap = join(splitDir, os.path.basename(mergedMap))
         print '-'*10,'spliting all_{0}_skeletonised'.format(args.measure),'-'*10
-        #os.system('fslsplit {0} {1}/split -t'.format(
-            #copied_mergedMap, splitDir))
+        os.system('fslsplit {0} {1}/split -t'.format(
+            copied_mergedMap, splitDir))
 
         # Name change
         mapList = [x for x in os.listdir(splitDir) if x.startswith('split')]
@@ -62,11 +67,15 @@ def main(args):
 
     df = df.sort_index()
     df['timeline'] = df.index.str.split('_').str[1]
+    df.index = df.index.str.split('_').str[0]
+
     cols = df.columns.tolist()
     cols = cols[-1:] + cols[:-1]
     df = df[cols]
-
-    df.to_csv('ROI_{0}.csv'.format(args.measure))
+    df.to_csv('Template_specific_ROI_{0}.csv'.format(args.measure))
+    newDf = df.reset_index()
+    newDf = newDf.groupby(['index','timeline']).mean().unstack('timeline')
+    newDf.to_csv('Template_specific_ROI_{0}_rearranged.csv'.format(args.measure))
 
     print '-'*10,'Completed','-'*10
 
@@ -92,6 +101,27 @@ def makeDirectory(directory):
         pass
 
 
+def makeROIs(roiDirectory):
+    fslDir=os.environ['FSLDIR']
+    fslAtlasesDir = join(fslDir, 'data', 'atlases')
+    JHU_label_1mm = join(fslAtlasesDir, 'JHU', 'JHU-ICBM-labels-1mm.nii.gz')
+    JHU_label_1mm_nb = nb.load(JHU_label_1mm)
+    JHU_label_1mm_data = JHU_label_1mm_nb.get_data()
+    JHU_label_xml = join(fslAtlasesDir, 'JHU-labels.xml')
+
+    with open(JHU_label_xml, 'r') as f:
+        lines = f.readlines()
+    searchedLines = [re.search('index="(\d+)".*>([A-Za-z \(\)\/-]+)<',x) for x in lines]
+    layer_num_name = dict([(x.group(1), x.group(2)) for x in searchedLines if x!=None])
+
+
+    for roiNum in range(1, JHU_label_1mm_data.max()):
+        roiName = re.sub('[^A-Za-z0-9\- ]+', '_', layer_num_name[str(roiNum)])
+        re.sub('[^A-Za-z0-9]+', '', mystring)
+        mask_data = JHU_label_1mm_data[JHU_label_1mm_data == roiNum]
+        nb.Nifti1Image(mask_data, JHU_label_1mm_nb.affine).to_filename(join(roiDirectory,
+                                                                            roiName))
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -106,6 +136,10 @@ if __name__=='__main__':
         help='Data directory location, default=pwd',
         default=os.getcwd())
     parser.add_argument(
+        '-r', '--roidir',
+        help='ROI directory location, default="ROIs"',
+        default=os.getcwd()+'/ROIs')
+    parser.add_argument(
         '-m', '--measure',
         help='"FA", "AD", "MD" or "RD"',
         default="FA")
@@ -113,6 +147,10 @@ if __name__=='__main__':
         '-c', '--core',
         help='Number of cores to use',
         default=8)
+    parser.add_argument(
+        '-t', '--template',
+        help='Template image, "MNI" or "others"',
+        default='MNI')
     args = parser.parse_args()
 
     if not args.dir:
